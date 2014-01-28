45fa553cc9c681269e5283002b70057bf495aca4
Abel Martin <abel.martin@gmail.com>
2014-01-28 01:55:22 -0500

    Added my thoughts on the examples I need to finish.

--- examples/01_02.clj
+++ examples/01_02.clj
@@ -1,4 +1,8 @@
 ; Exercise 1.2.13
+  ; Thinking about this explanation makes my hair hurt.  Possibly from my brain catching fire.
 ; Exercise 1.2.14
+  ; Fun?
 ; Exercise 1.2.17
+  ; Neat!
 ; Exercise 1.2.24
+  ; ???
